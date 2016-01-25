71c4408aa671b206c7244cfe378e17d2a67cf05c
Andy Kee <andykee@gmail.com>
2016-01-25 15:22:48 -0800

    Add brew.sh

---
+++ extras/brew.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install vim
+brew install vim
+
+# Install python 2 and 3
+brew install python
+brew install python3
